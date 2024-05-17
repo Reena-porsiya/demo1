18775282f86bf333013249d48abfd4d80eadcdcc
Reena-porsiya <138755077+Reena-porsiya@users.noreply.github.com>
2024-05-17 12:38:24 +0530

    c

--- mysamples/Stream/src/com/FindEndWith.java
+++ mysamples/Stream/src/com/FindEndWith.java
@@ -2,6 +2,7 @@ package com;
 
 import java.util.stream.Stream;
 
+
 public class FindEndWith {
 	public static void main(String[] args) {
 	Stream<String> stream = Stream.of("Geeks", "fOr", "GEEKSQUIZ", "GeeksforGeeks");
